2e61578896e99f4f8ca291047c533c0618cc8fb6
Christopher Yammine <christophery@mogo.ca>
2017-03-14 15:06:47 -0700

    Initial commit + dependencies

---
+++ lib/ex_zen_hub.ex
@@ -0,0 +1,18 @@
+defmodule ExZenHub do
+  @moduledoc """
+  Documentation for ExZenHub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExZenHub.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_zen_hub_test.exs
@@ -0,0 +1,8 @@
+defmodule ExZenHubTest do
+  use ExUnit.Case
+  doctest ExZenHub
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
