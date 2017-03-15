7d02b24132f93079221498e8e14cbe19d3536c91
Christopher Yammine <christophery@mogo.ca>
2017-03-15 10:13:16 -0700

    Fix compiler warnings

--- lib/ex_zen_hub/http.ex
+++ lib/ex_zen_hub/http.ex
@@ -5,8 +5,9 @@ defmodule ExZenHub.HTTP do
   See https://hexdocs.pm/httpoison/HTTPoison.html#request/5 for list of `opts`
   """
 
-  @spec request(String.t, :get | :post | :put | :patch | :delete, String.t, Keyword.t, Keyword.t) :: HTTPoison.Response.t | HTTPoison.Error.t
-  def request({:ok, auth_token}, method, url, params, opts \\ []) do
+  @spec request({:ok | :error, String.t | ExZenHub.Error.t}, :get | :post | :put | :patch | :delete, String.t, Keyword.t, Keyword.t) :: {:ok, HTTPoison.Response.t} | {:error, HTTPoison.Error.t}
+  def request({:error, _} = err, _, _, _, _), do: err
+  def request({:ok, auth_token}, method, url, params, opts) do
     authorization_header = {"X-Authentication-Token", auth_token}
     # probably some prep here
     case method do
@@ -16,5 +17,4 @@ defmodule ExZenHub.HTTP do
         HTTPoison.request(method, url, {:form, params}, [authorization_header], opts)
     end
   end
-  def request({:error, _} = err, _, _, _, _), do: err
 end
