b7c80d4a49ddf5ba3d5331b2088fc9bdc95b9290
Christopher Yammine <christophery@mogo.ca>
2017-03-15 14:59:50 -0700

    Bump to version 0.1.1: - Modify app name in mix.exs - Remove boilerplate comments in mix.exs

--- mix.exs
+++ mix.exs
@@ -2,8 +2,8 @@ defmodule ExZenHub.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ex_zen_hub,
-     version: "0.1.0",
+    [app: :ex_zenhub,
+     version: "0.1.1",
      elixir: "~> 1.4",
      description: "Unofficial Elixir client for the ZenHub API",
      build_embedded: Mix.env == :prod,
@@ -14,23 +14,10 @@ defmodule ExZenHub.Mixfile do
      package: package()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:poison, "~> 3.0"},
